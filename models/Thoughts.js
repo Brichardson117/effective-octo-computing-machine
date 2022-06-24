a25c113af84351fb3160d1f2f8fcf199468a157c
Brittney <brittneyrichardson8@gmail.com>
2022-06-23 22:00:26 -0500

    completed models for User and Thoughts

---
+++ models/Thoughts.js
@@ -0,0 +1,67 @@
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: "Explain your reaction",
+      maxLength: [280, "Woah, too much!"],
+    },
+    userName: {
+      type: String,
+      required: "Name is required for who is leaving the reaction",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAt) => dateFormat(createdAt),
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const thoughtsSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "Complete your thought!",
+      minLength: [1, "Add more!"],
+      maxLength: [280, "Woah, thats too much"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    userName: {
+      type: String,
+      required: "Who thought is required!",
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtsSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thoughts = model("Thoughts", thoughtsSchema);
+
+module.exports = Thoughts;
